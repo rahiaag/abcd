8df0fa8a5a4408e794a586efc99ae00561c4a3df
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2024-12-24 00:14:55 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+      priority_queue<int>pq;
+      for (int i:arr){
+          pq.push(i);
+      }
+      return pq.top();
+    }
+};
+
+//priority queue by default max heap form karta hai
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        string input;
+        int num;
+        vector<int> arr;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.largest(arr) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
