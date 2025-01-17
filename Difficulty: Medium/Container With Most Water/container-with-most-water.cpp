8acbbab91f97305488970446056874ec537f673d
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2025-01-17 17:28:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.cpp
@@ -9,30 +9,42 @@ class Solution {
 
   public:
     int maxWater(vector<int> &arr) {
-        
         int n=arr.size();
+        int low=0;
+        int high=n-1;
+        int res=0;
+        
+        while (low<high){
+           int temp=min(arr[low],arr[high])*(high-low);
+            res=max(temp,res);
+            
+            if (arr[low]<arr[high]){
+                low++;
+            }
+            else{
+                high--;
+            }
+        }
+        return res;
+       
+    }
+};
+
+/* int n=arr.size();
         if (n<=1){
             return 0;
         }
-        int left=0;
-        int right=n-1;
         int maxElement=0;
         int sum=0;
+
+        for (int i=0;i<n-1;i++){
+            for (int j=i+1;j<n;j++){
         
-        while (left<right){
-            sum=min(arr[right],arr[left])*(right-left);
-            maxElement=max(sum,maxElement);
-            
-            if (arr[left]<arr[right]){
-                left++;
-            }
-            else{
-                right--;
+                sum=(j-i)*min(arr[i],arr[j]);
+                maxElement=max(maxElement,sum);
             }
         }
-        return maxElement;
-    }
-};
+        return maxElement;*/
 
 //{ Driver Code Starts.
 int main() {
