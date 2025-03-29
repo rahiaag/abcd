13dd70e6104b1f3855044bce55aeff2dfe7a67e4
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2025-03-29 15:22:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted with Duplicates/union-of-2-sorted-with-duplicates.cpp
+++ Difficulty: Medium/Union of 2 Sorted with Duplicates/union-of-2-sorted-with-duplicates.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 // } Driver Code Ends
 
+/*
 class Solution {
   public:
     // a,b : the arrays
@@ -21,14 +22,49 @@ class Solution {
         
         for (int i=0;i<b.size();i++){
             if (st.find(b[i])==st.end()){
-                st.insert(b[i]);
+                
             }
         }
-        vector<int>ans;
-        for (auto i:st){
-            ans.push_back(i);
+    }
+};*/
+class Solution {
+  public:
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        vector<int> result;
+        int i = 0, j = 0;
+        int n = a.size(), m = b.size();
+        
+        while (i < n && j < m) {
+            if (a[i] < b[j]) {
+                if (result.empty() || result.back() != a[i]) 
+                    result.push_back(a[i]);
+                i++;
+            } 
+            else if (a[i] > b[j]) {
+                if (result.empty() || result.back() != b[j]) 
+                    result.push_back(b[j]);
+                j++;
+            } 
+            else { // If both are equal, insert only once
+                if (result.empty() || result.back() != a[i]) 
+                    result.push_back(a[i]);
+                i++, j++;
+            }
         }
-        return ans;
+        
+        // Insert remaining elements
+        while (i < n) {
+            if (result.empty() || result.back() != a[i]) 
+                result.push_back(a[i]);
+            i++;
+        }
+        while (j < m) {
+            if (result.empty() || result.back() != b[j]) 
+                result.push_back(b[j]);
+            j++;
+        }
+        
+        return result;
     }
 };
 
