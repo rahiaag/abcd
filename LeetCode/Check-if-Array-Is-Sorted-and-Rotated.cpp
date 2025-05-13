5f161f7cec3313dc7c8966867c11907cc1077623
Rahi Agarwal <135974912+rahiaag@users.noreply.github.com>
2025-05-14 00:45:54 +0530

    [1752] [Time Beats: 100.00%] [Memory Beats: 45.56%] - LeetPush

---
+++ LeetCode/Check-if-Array-Is-Sorted-and-Rotated.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool check(vector<int>& arr) {
+        int n=arr.size();
+        int count=0;
+
+        for (int i=0;i<n-1;i++){
+            if (arr[i]>arr[i+1]){
+                count++;
+            }
+        }
+        if (arr[n-1]>arr[0]){
+            count++;
+        }
+        if (count>1){
+            return false;
+        }
+        return true;
+    }
+};
